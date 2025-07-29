dd181868b093417748f85ce032597627156daee0
morteerror404 <frotatv@gmail.com>
2025-07-29 20:49:08 -0300

    adicionando preset da aplicação web

---
+++ Modules/Web Dash/components/TestingPage.jsx
@@ -0,0 +1,19 @@
+                          >
+                            <Copy className="w-3 h-3" />
+                          </Button>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
+}
+
+export default TestingPage
+
